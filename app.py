49ffffad7222368989f2aae0d924c17361603843
Elijah Rogers <119324352+jusssEli@users.noreply.github.com>
2024-05-12 21:31:52 -0400

    Update app.py
    Eli's python code pt2 with the help of Avery who also did both meetings

--- app.py
+++ app.py
@@ -98,3 +98,42 @@ def listing():
     print(data)
     #return f"Done!! Query Result is {data}"
     return render_template('listing.html', data=data)
+def form():
+    return render_template('form.html')
+ 
+@app.route('/search', methods = ['POST', 'GET'])
+def search():
+    if request.method == 'GET':
+        return "Fill out the Search Form"
+
+    if request.method == 'POST':
+        name = request.form['name']
+        emp_id = request.form['emp_id']
+        cursor = mysql.connection.cursor()
+        if name:
+            cursor.execute("SELECT * from employee where name = %s",[name])
+        if emp_id:
+            cursor.execute("SELECT * from employee where emp_id = %s",[emp_id])
+        mysql.connection.commit()
+        data = cursor.fetchall()
+        cursor.close()
+        print(data)
+        #return f"Done!! Query Result is {data}"
+        return render_template('results.html', data=data)
+
+@app.route('/meetings')
+def meetings():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM meeting")
+    meeting_data = cursor.fetchall()
+    cursor.close()
+    return render_template('meetings.html', meetings=meeting_data)
+
+@app.route('/remove_meeting/<int:meeting_id>', methods=['POST'])
+def remove_meeting(meeting_id):
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        cursor.execute("DELETE FROM meeting WHERE meeting_id = %s", [meeting_id])
+        mysql.connection.commit()
+        cursor.close()
+        return redirect(url_for('meetings'))
